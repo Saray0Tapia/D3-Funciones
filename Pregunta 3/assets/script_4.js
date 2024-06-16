27d52b70a726383c09be2c806c01f918f37ce165
saray <saraytapia0@gmail.com>
2024-06-16 17:25:39 -0400

    estructura negro

--- Pregunta 3/assets/script_4.js
+++ Pregunta 3/assets/script_4.js
@@ -1,30 +1,11 @@
-function pintar(ele1,color='blue'){
-    ele1.style.backgroundColor = color
-    
-}
-ele1 = document.getElementById("#uno")
-ele1.addEventListener("click",()=>{
-    pintar(ele1,"black")
-});
-
-
-function pintar(color){
-    let ele2 = document.querySelector("#dos")
-    ele.style.backgroundColor = color
-}
+let ele1 = document.querySelector("#uno")
 let ele2 = document.querySelector("#dos")
-ele.addEventListener('click', pintar("black"));
-
-function pintar(color){
-    let ele3 = document.querySelector("#tres")
-    ele.style.backgroundColor = color
-}
 let ele3 = document.querySelector("#tres")
-ele.addEventListener('click', pintar("black"));
-
-function pintar(color){
-    let ele4 = document.querySelector("#cuatro")
-    ele.style.backgroundColor = color
-}
 let ele4 = document.querySelector("#cuatro")
-ele.addEventListener('click', pintar("black"));
+function pintarBlack(event){
+    event.target.style.backgroundColor = "black"
+}
+ele3.addEventListener("click", pintarBlack)
+ele4.addEventListener("click", pintarBlack)
+ele2.addEventListener("click", pintarBlack)
+ele1.addEventListener("click", pintarBlack)
